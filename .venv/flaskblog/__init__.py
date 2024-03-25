71bf5a82a711cef9d3859a08b91de5235d55cf93
Austin <amusuya57@gmail.com>
2024-03-25 13:56:08 +0300

    Add new files and directories

---
+++ .venv/flaskblog/__init__.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '9fa3c47a2206a43a2391de6f73e28c26'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+# app password = xinx molj ezqv wxxs
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+# app.config['MAIL_PASSWORD'] = 'xinx molj ezqv wxxs'
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+from flaskblog import routes, models
+
+# Create database tables within the application context
+with app.app_context():
+    db.create_all()
